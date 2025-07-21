0560e988637bf4faba721565fa44ed4841dd4595
shoaib bhatti <shoaiburrehman4447@gmail.com>
2025-07-21 16:26:44 +0500

    Task creation project initialize with fluent UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import * as React from "react";
+import {  Button } from "@fluentui/react-components";
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+      <div style={{ padding: "2rem" }}>
+        <h1>Hello Fluent UI</h1>
+        <Button appearance="primary">Click Me</Button>
+        <Button appearance="outline">Click Me</Button>
+        <Button appearance="subtle">Click Me</Button>
+        <Button appearance="ghost">Click Me</Button>
+        <Button appearance="link">Click Me</Button>
+      </div>
+  );
 }
 
-export default App
+export default App;
